a477b5643bee5de31653d51c29fe5a425a8a4bb0
fateevv85 <fateevv85@gmail.com>
2018-06-05 16:15:08 +0300

    task done

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php
+define('USER_LOGIN', 'connor_84@inbox.ru');
+define('USER_HASH', '43dbd8bfdbc811c790947f0d39ec0541');
+define('SUBDOMAIN', 'connor84');
+define('LINK', 'https://' . SUBDOMAIN . '.amocrm.ru/api/v2');
+
+#Авторизуемся
+function login()
+{
+    $user = array(
+        'USER_LOGIN' => USER_LOGIN,
+        'USER_HASH' => USER_HASH
+    );
+
+#Формируем ссылку для запроса
+    $link = 'https://' . SUBDOMAIN . '.amocrm.ru/private/api/auth.php?type=json';
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+    curl_setopt($curl, CURLOPT_URL, $link);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($user));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt');
+    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt');
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    $out = curl_exec($curl);
+    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    $code = (int)$code;
+    $errors = array(
+        301 => 'Moved permanently',
+        400 => 'Bad request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not found',
+        500 => 'Internal server error',
+        502 => 'Bad gateway',
+        503 => 'Service unavailable'
+    );
+    try {
+        if ($code != 200 && $code != 204)
+            throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
+    } catch (Exception $E) {
+        die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+    }
+
+    $Response = json_decode($out, true);
+    $Response = $Response['response'];
+
+    #Если авторизация успешна, возвращаем true
+    return (isset($Response['auth'])) ? true : 0;
+}
+
+#Ищем сделки без активных задач
+function findLeadsWithoutTasks()
+{
+    $link = LINK . '/leads?limit_rows=50';
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+    curl_setopt($curl, CURLOPT_URL, $link);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('IF-MODIFIED-SINCE: Mon, 01 Aug 2013 07:07:23'));
+
+    $out = curl_exec($curl);
+    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    $code = (int)$code;
+    $errors = array(
+        301 => 'Moved permanently',
+        400 => 'Bad request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not found',
+        500 => 'Internal server error',
+        502 => 'Bad gateway',
+        503 => 'Service unavailable'
+    );
+    try {
+        if ($code != 200 && $code != 204) {
+            throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
+        }
+    } catch (Exception $E) {
+        die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+    }
+
+    $Response = json_decode($out, true);
+    $Response = $Response['_embedded']['items'];
+
+    /* Добавляем в массив только те сделки, у которых нет открытых задач  */
+    foreach ($Response as $key) {
+        if (!$key['closest_task_at']) {
+            $leads_without_tasks[] = $key;
+        }
+    }
+    return $leads_without_tasks;
+}
+
+#Добавляем задачи в сделки
+function tasks($leads)
+{
+    foreach ($leads as $key) {
+        $tasks['add'][] = [
+            'element_id' => $key['id'], #ID сделки
+            'element_type' => 2, #Показываем, что это - сделка, а не контакт
+            'task_type' => 3, #Написать письмо
+            'text' => 'Сделка без задачи',
+            'responsible_user_id' => $key['responsible_user_id'],
+            'complete_till_at' => mktime(date('23-59-59 m-d-Y'))
+        ];
+    }
+
+    $link = LINK . '/tasks';
+
+    $curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+#Устанавливаем необходимые опции для сеанса cURL
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+    curl_setopt($curl, CURLOPT_URL, $link);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($tasks));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    /* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+    $code = (int)$code;
+    $errors = array(
+        301 => 'Moved permanently',
+        400 => 'Bad request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not found',
+        500 => 'Internal server error',
+        502 => 'Bad gateway',
+        503 => 'Service unavailable'
+    );
+    try {
+        #Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
+        if ($code != 200 && $code != 204)
+            throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
+    } catch (Exception $E) {
+        die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+    }
+}
+
+if (login() && findLeadsWithoutTasks()) {
+    tasks(findLeadsWithoutTasks());
+    echo 'tasks is added';
+}
